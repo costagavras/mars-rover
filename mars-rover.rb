abe76c9030e2e535fd061b3b0ec781e0c6bddcb0
costagavras <cgavras@yahoo.ca>
2018-12-13 22:42:19 -0500

    rover_instructions

---
+++ mars-rover.rb
@@ -0,0 +1,2 @@
+# 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot.
+# 'M' means move forward one grid point, and maintain the same heading.
