a44e721da54028db59008eded1627bd83d26adcf
costagavras <cgavras@yahoo.ca>
2018-12-13 22:48:52 -0500

    initialize

--- mars-rover.rb
+++ mars-rover.rb
@@ -1,2 +1,10 @@
 # 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot.
 # 'M' means move forward one grid point, and maintain the same heading.
+
+class Rover
+
+  def initialize
+    
+  end
+
+end
