fe3912309a2e4821df1a6548e10ff36b24f628a9
costagavras <cgavras@yahoo.ca>
2018-12-14 19:10:58 -0500

    basic_commands

--- mars-rover.rb
+++ mars-rover.rb
@@ -11,7 +11,57 @@ class Rover
     @direction = direction
   end
 
-  
+  def read_instruction(instructions)
+    ar_instructions = []
+    ar_instruction = instructions.char.to_a
+    ar_instruction.each do |cmd|
+      if cmd = "M"
+        move
+      elsif cmd = "R" || cmd = "L"
+        turn(cmd)
+      end
+    end
+    print rover
+  end
+
+  def move
+    case rover.direction
+    when "N"
+      rover.y_coord +=1
+    when "S"
+      rover.y_coord -=1
+    when "E"
+      rover.x_coord +=1
+    when "W"
+      rover.x_coord -=1
+    end
+  end
+
+  def turn(cmd)
+    if cmd = "L"
+      case rover.direction
+        when "N"
+          rover.direction = "W"
+        when "S"
+          rover.direction = "E"
+        when "E"
+          rover.direction = "N"
+        when "W"
+          rover.direction = "S"
+      end
+    elsif cmd = "R"
+        case rover.direction
+        when "N"
+          rover.direction = "E"
+        when "S"
+          rover.direction = "W"
+        when "E"
+          rover.direction = "S"
+        when "W"
+          rover.direction = "N"
+        end
+    end
+  end
 
 end
 
